edb4fdd2aca6b92bc1ad0953abcb031a5994b8ee
vicey3n <vicey3n@WDAUD7210FGY.internal.vic.gov.au>
2018-05-28 10:37:09 +1000

    Reposition of elements.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -11,15 +11,15 @@ import Books from './pages/Books'
 const App = () =>
   <Router>
     <div>
-      <ul>
+    <Route exact path="/" component={Home}/>
+    <Route path="/books" component={Books}/>
+    
+    <ul>
         <li><Link to="/">Home</Link></li>
         <li><a href="/about">About</a></li> {/* Note that this is still an anchor */}
         <li><Link to="/books">Books</Link></li>
       </ul>
 
-
-      <Route exact path="/" component={Home}/>
-      <Route path="/books" component={Books}/>
     </div>
   </Router>
 
