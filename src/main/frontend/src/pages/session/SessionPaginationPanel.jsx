2f6e04a039f203fa6076a4b57edfd0ede6fd6f47
Jiang Liu (DEDJTR) <jiang.liu@ecodev.vic.gov.au>
2018-06-01 15:58:53 +1000

    Added pagination render functionality.

--- src/main/frontend/src/pages/session/SessionPaginationPanel.jsx
+++ src/main/frontend/src/pages/session/SessionPaginationPanel.jsx
@@ -2,6 +2,38 @@ import React, { Component } from 'react'
 
 export default class SessionPaginationPanel extends Component {
     render() {
-        return <div className="pagination_center">1 2 3 4 5 &emsp; Next</div>
+        let paginationPanel;
+        if ((undefined !== this.props.totalPageNo)
+            && (null !== this.props.totalPageNo)) {
+                const len = (this.props.totalPageNo < 10) ? this.props.totalPageNo : 10;
+                const pageNumbers = [];
+                for (let i = 1; i <= len; i++) {
+                    pageNumbers.push(i);
+                }
+
+                const renderPageNumbers = pageNumbers.map(number => {
+                    return(
+                        <li
+                            key={number}
+                            id={number}
+                        >
+                            {number}
+                        </li>    
+                    );
+                });
+                paginationPanel = <ul id="page-numbers">
+                                    {renderPageNumbers} 
+                                    &emsp; 
+                                    <li>
+                                        Next
+                                    </li>
+                                </ul>
+        }
+        
+        return (
+            <div className="pagination_center">
+                {paginationPanel}
+            </div>
+        );
     }
 }
