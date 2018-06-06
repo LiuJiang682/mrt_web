838f0f58bfb421f175a5b97dd3f8ea1ed5f40521
vicey3n <vicey3n@WDAUD7210FGY.internal.vic.gov.au>
2018-06-06 12:10:11 +1000

    Added Rest repository interface.

--- src/main/java/au/gov/vic/ecodev/mrt/web/repository/rest/SessionHeaderRepository.java
+++ src/main/java/au/gov/vic/ecodev/mrt/web/repository/rest/SessionHeaderRepository.java
@@ -2,6 +2,7 @@ package au.gov.vic.ecodev.mrt.web.repository.rest;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -12,5 +13,6 @@ import au.gov.vic.ecodev.mrt.model.SessionHeader;
 public interface SessionHeaderRepository extends PagingAndSortingRepository<SessionHeader, Long> {
 
 	@RestResource
-	List<SessionHeader> findAll();
+	@Query(value = "SELECT * FROM SESSION_HEADER s WHERE s.APPROVED = 0 AND s.REJECTED = 0 ORDER BY s.CREATED desc", nativeQuery=true)
+	List<SessionHeader> findByNotApprovedAndNotRejected();
 }
