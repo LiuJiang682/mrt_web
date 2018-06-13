8086f8148b4e3231f82395edd16a37e8dfaede03
vicey3n <vicey3n@WDAUD7210FGY.internal.vic.gov.au>
2018-06-13 15:32:02 +1000

    Added SurfaceGeochemistryRepository class.

---
+++ src/main/java/au/gov/vic/ecodev/mrt/web/repository/rest/SurfaceGeochemistryRepository.java
@@ -0,0 +1,19 @@
+package au.gov.vic.ecodev.mrt.web.repository.rest;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import au.gov.vic.ecodev.mrt.model.sg4.SurfaceGeochemistry;
+
+@RepositoryRestResource(collectionResourceRel = "surfaceGeochemistry", path = "surfaceGeochemistry")
+public interface SurfaceGeochemistryRepository extends PagingAndSortingRepository<SurfaceGeochemistry, Long> {
+
+	@RestResource(path="get")
+	@Query("SELECT s FROM SurfaceGeochemistry s WHERE s.loaderId = :loaderId")
+	List<SurfaceGeochemistry> findByLoaderId(@Param("loaderId") long loaderId);
+}
