752623e22277fe399e4b95a748c8a6c41705c9e5
vicey3n <vicey3n@WDAUD7210FGY.internal.vic.gov.au>
2018-06-18 09:07:45 +1000

    Added File Log rest repository.

---
+++ src/main/java/au/gov/vic/ecodev/mrt/web/repository/rest/FileErrorLogRepository.java
@@ -0,0 +1,19 @@
+package au.gov.vic.ecodev.mrt.web.repository.rest;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import au.gov.vic.ecodev.mrt.model.FileLog;
+
+@RepositoryRestResource(collectionResourceRel = "fileLogs", path = "fileLogs")
+public interface FileErrorLogRepository extends PagingAndSortingRepository<FileLog, Long> {
+
+	@RestResource(path="get")
+	@Query("SELECT f FROM FileLog f WHERE f.batchId = :loaderId")
+	List<FileLog> findByLoaderId(@Param("loaderId") long loaderId);
+}
