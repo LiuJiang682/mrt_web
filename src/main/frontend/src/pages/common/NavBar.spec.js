9aefb7d210ae9d155e7d50a7edf58ab173b01166
Jiang Liu (DEDJTR) <jiang.liu@ecodev.vic.gov.au>
2018-08-31 13:13:36 +1000

    Update the test case.

--- src/main/frontend/src/pages/common/NavBar.spec.js
+++ src/main/frontend/src/pages/common/NavBar.spec.js
@@ -7,7 +7,7 @@ import NavBar from './NavBar';
 
 Enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
-const expectedHtml = '<div><nav class="navbar navbar-default"><div class="container-fluid"><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><ul class="nav navbar-nav"><li><a activeclassname="active" href="/">Session Summary</a></li></ul></div></div></nav></div>';
+const expectedHtml = '<div><nav class="navbar navbar-default"><div class="container-fluid"><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><ul class="nav navbar-nav"><li><a class="active" href="/">Session Summary</a></li></ul></div></div></nav></div>';
 
 describe('NavBar', () => {
     it('should render NavBar', () => {
